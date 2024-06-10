4a700fddbb9e9d4142d9b017ed8fba5929e09e33
Duy Node <duyle.sales@gmail.com>
2024-06-10 14:30:49 +0700

    Create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        nesciunt ducimus hic necessitatibus obcaecati vitae qui suscipit optio
+        iusto distinctio, dolor temporibus ipsa aliquid culpa, reprehenderit
+        deserunt omnis exercitationem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        nostrum at quam ad odio possimus. Ipsa similique accusamus numquam, nisi
+        quis, at perferendis fugiat illum quaerat animi quidem aliquid saepe
+        error non vero molestias dolore labore odio? Provident perspiciatis
+        delectus minima fugit beatae assumenda dignissimos nulla? Non cum enim
+        doloribus quod, quis, cupiditate dolorum et repudiandae laboriosam
+        veritatis quia suscipit!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+        voluptatibus? Repudiandae numquam modi temporibus possimus nihil veniam
+        distinctio deleniti officiis nostrum debitis itaque, aliquid corrupti
+        harum hic accusamus laborum dolore.
+      </p>
+    </div>
+  );
+}
